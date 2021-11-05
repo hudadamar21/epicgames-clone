0ceb2b13102edefeaee364fbd6288716d935a1de
Huda Damar <hudadamar21@gmail.com>
2021-11-05 20:01:01 +0700

    fix checkbox

--- src/components/atom/Checkbox.vue
+++ src/components/atom/Checkbox.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { ref } from "vue"
 import { Switch, SwitchGroup, SwitchLabel } from "@headlessui/vue";
-defineProps<{
+const props = defineProps<{
   value: boolean;
 }>();
+
+const checkValue = ref(props.value)
 </script>
 
 <template>
@@ -10,11 +13,11 @@ defineProps<{
     <div class="grid items-center justify-start grid-flow-col">
       <div class="w-5 h-5">
         <Switch
-          v-model="value"
-          :class="value ? 'bg-epic-blue border-black' : 'bg-transparent'"
+          v-model="checkValue"
+          :class="checkValue ? 'bg-epic-blue border-black' : 'bg-transparent'"
           class="grid w-full h-full border ring-8 ring-transparent hover:ring-white/10 place-items-center focus:outline-none"
         >
-          <CheckIcon v-show="value" />
+          <CheckIcon v-show="checkValue" />
         </Switch>
       </div>
       <SwitchLabel class="flex-grow ml-4 cursor-pointer">
